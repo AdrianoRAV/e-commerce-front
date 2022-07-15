3dafd1709591d8ca406241c7f5262dce835943d2
adriano <adrianoskte@gmail.com>
2022-07-15 00:41:32 -0300

    mascara de senha

--- lib/src/auth/components/custom_tex_field.dart
+++ lib/src/auth/components/custom_tex_field.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final IconData icon;
   final String label;
-  final bool isObscure;
+  final bool isSecret;
+  
 
   const CustomTextField({
     Key? key,
-    this.isObscure = false,
+    this.isSecret = false,
     required this.icon,
     required this.label,
   }) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+
+   bool isObscure = false;
+
+   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    isObscure = widget.isSecret;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +39,17 @@ class CustomTextField extends StatelessWidget {
       child: TextFormField(
         obscureText: isObscure,
         decoration: InputDecoration(
-          prefixIcon: Icon(icon),
-          labelText: label,
+          prefixIcon: Icon(widget.icon),
+          suffixIcon: widget.isSecret ? IconButton(
+            onPressed:(){
+              setState(() {
+                isObscure = !isObscure;
+              });
+              
+            },
+            icon:  Icon(isObscure ? Icons.visibility : Icons.visibility_off),
+             ): null,
+          labelText: widget.label,
           isDense: true,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(18),

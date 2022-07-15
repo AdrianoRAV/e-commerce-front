fa7d11d7e52f21ad681ad28d62fae880fef863a9
adriano <adrianoskte@gmail.com>
2022-07-14 23:54:23 -0300

    first commit

---
+++ lib/src/auth/components/custom_tex_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CustomTextField extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final bool isObscure;
+
+  const CustomTextField({
+    Key? key,
+    this.isObscure = false,
+    required this.icon,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: TextFormField(
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          labelText: label,
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+      ),
+    );
+  }
+}

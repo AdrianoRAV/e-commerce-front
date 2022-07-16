1615973252d61b2cb2b374a58d0f1a66cbe13591
adriano <adrianoskte@gmail.com>
2022-07-15 22:52:51 -0300

    mascara cpf, telefone e botão voltar

--- lib/src/auth/components/custom_tex_field.dart
+++ lib/src/auth/components/custom_tex_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -7,6 +8,7 @@ class CustomTextField extends StatefulWidget {
   final IconData icon;
   final String label;
   final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
   
 
   const CustomTextField({
@@ -14,6 +16,7 @@ class CustomTextField extends StatefulWidget {
     this.isSecret = false,
     required this.icon,
     required this.label,
+    this.inputFormatters,
   }) : super(key: key);
 
   @override
@@ -37,6 +40,7 @@ class _CustomTextFieldState extends State<CustomTextField> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 15),
       child: TextFormField(
+        inputFormatters: widget.inputFormatters,
         obscureText: isObscure,
         decoration: InputDecoration(
           prefixIcon: Icon(widget.icon),
--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,99 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:quitanda/src/config/custom_colors.dart';
 
 import 'components/custom_tex_field.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+  SignUpScreen({Key? key}) : super(key: key);
 
-  @override
+  final cpfFormatter = MaskTextInputFormatter(
+    mask: '###.###.###-##',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+final phoneFormatter = MaskTextInputFormatter(
+    mask: '## # ####-#### ',
+    filter: {"#": RegExp(r'[0-9]')},
+  );
+
+
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children:  [
-         const  Expanded(
-            child: Center(
-              child: Text('Cadastro',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                  )),
-            ),
-          ),
-          //Fomulario
-           Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(45),
-                    )),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    //Email
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: 'Email',
-                    ),
-
-                    //Senha
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: 'Senha',
-                      isSecret: true,
-                    ),
-                   
-                    //Nome
-                      const CustomTextField(
-                      icon: Icons.person,
-                      label: 'Nome',
-                    ),
-                    
-                  // Celular 
-  const CustomTextField(
-                      icon: Icons.phone,
-                      label: 'Celular',
-                    ),
-                    // Cpf
-  const CustomTextField(
-                      icon: Icons.contact_page,
-                      label: 'Cpf',
-                    ),
-                    // Botão entrar
-                    SizedBox(
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          'Cadastrar Usuário',
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text('Cadastro',
                           style: TextStyle(
-                            fontSize: 18,
                             color: Colors.white,
+                            fontSize: 35,
+                          )),
+                    ),
+                  ),
+                  //Fomulario
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(45),
+                        )),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        //Email
+                        const CustomTextField(
+                          icon: Icons.email,
+                          label: 'Email',
+                        ),
+
+                        //Senha
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+
+                        //Nome
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: 'Nome',
+                        ),
+
+                        // Celular
+                         CustomTextField(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                          inputFormatters: [phoneFormatter],
+                        ),
+                        // Cpf
+                         CustomTextField(
+                          icon: Icons.file_copy,
+                          label: 'Cpf',
+                          inputFormatters: [cpfFormatter],
+                        ),
+                        // Botão entrar
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar Usuário',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                   
-
-
-
-
-                  ],
-                ),
+                  ),
+                ],
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: SafeArea(
+                    child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                )),
               ),
             ],
+          ),
+        ),
       ),
     );
   }
